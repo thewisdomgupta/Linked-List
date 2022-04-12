52d00342db0029acce11cfe233e2b10ca721a7de
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-04-12 11:09:24 +0530

    Constructor Printing No Value via LinkedList

---
+++ Singly Linked List/PrintNoValue.java
@@ -0,0 +1,12 @@
+public class PrintNoValue {
+    public static void main(String [] args){
+        Node obj=new Node();
+        System.out.println(obj.x);
+        System.out.println(obj.next);
+
+    }
+}
+class Node{
+    int x;
+    Node next;
+}
