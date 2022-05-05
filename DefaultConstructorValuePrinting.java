74355a500766c1b2a3a73051f6e709c5a53dc717
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-05-05 21:33:07 +0530

    Add files via upload

---
+++ DefaultConstructorValuePrinting.java
@@ -0,0 +1,12 @@
+public class DefaultConstructorValuePrinting {
+    public static void main(String[] args) {
+        Node obj = new Node();
+        System.out.println(obj.x);
+        System.out.println(obj.next);
+    }
+}
+    class Node{
+        int x;
+        Node next;
+    }
+
